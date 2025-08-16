cb9b7c16933e01a52d6867bc43eb031ab4750dad
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2025-08-16 15:13:29 +0530

    added frontend url in backend

--- server/server.js
+++ server/server.js
@@ -9,7 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://stock-market-dashboard-rho.vercel.app/", // frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // API Routes
